8cfa33e1f5b3c28da3cb0af6b0e394ef038c346a
Daniel <dhernandez8@ufl.edu>
2023-12-01 14:38:40 -0500

    updates

--- cell.py
+++ cell.py
@@ -1,3 +1,7 @@
+import pygame
+from const import *
+
+
 class Cell:
     def __init__(self, value, row, col, screen):
         self.value = value
@@ -14,5 +18,28 @@ class Cell:
         self.value = value
         self.value_is_sketched = True
 
+    '''
+    this calculates a grid with the grid location. If it's not a sketch, returns middle coords of box, else,
+    returns the top left coordinates of the box. Returns coordinates of the grid as a tuple
+    '''
+    @staticmethod
+    def _get_coordinates(row, column):
+        grid = []
+        for vert in range(9):  # iterates through columns
+            cur = []  # temporary list, will get appended to grid to do a 2D list
+            for horiz in range(9):  # iterates through rows
+                cur.append([25 + (vert * 50), 25 + (horiz * 50)])  # appends x and y coords to grid
+            grid.append(cur)
+        return tuple(grid[column][row])  # returns the coordinate of the given box as a tuple
+
+    '''this function draws the box outline as well as the value of the instance'''
     def draw(self):
-        screen.
+        if self.value == '0':  # don't print if the cell value is 0
+            return False
+        cell_coords = self._get_coordinates(self.row, self.col)  # gets coordinates where number will go
+        cell_box = pygame.Rect(cell_coords[0], cell_coords[1], 50, 50)  # create rectangle outline
+        pygame.draw.rect(self.screen, REGULAR_LINE_COLOR, cell_box, width=2)  # draws rectangle outline
+        cell_font = pygame.font.Font(None, 75)  # define font for number
+        cell_rendered = cell_font.render(self.value, True, GIVEN_NUMBER_COLOR)  # renders number
+        self.screen.blit(cell_rendered, cell_rendered.get_rect(center=(cell_coords[0]+25, cell_coords[1]+28)))  #display
+        return True
--- sudoku.py
+++ sudoku.py
@@ -4,31 +4,6 @@ import pygame, os
 from const import *  # imports constants from const.py
 from board import Board
 
-selected_cell = []  # stores the selected cell
-board = []  # stores the board
-
-
-'''
-this calculates a grid with the grid location. If it's not a sketch, returns middle coords of box, else,
-returns the top left coordinates of the box. Returns coordinates of the grid as a tuple
-'''
-
-def get_coordinates(horizontal, vertical, is_sketch):
-    grid = []
-    sketch_constant = 0 if is_sketch else 25  # whether it will find the midpoint of the box (box+half), or top left corner (box)
-    for vert in range(9):  # iterates through columns
-        cur = []  # temporary list, will get appended to grid to do a 2D list
-        for horiz in range(9):  # iterates through rows
-            cur.append([25+(vert*50)+sketch_constant, 25+(horiz*50)+sketch_constant])  # appends x and y coords to grid
-        grid.append(cur)
-
-    grid[vertical][horizontal][0] += 5 if is_sketch else 0  # horizontal adjustment for sketches only
-
-    return tuple(grid[vertical][horizontal])  # returns the coordinate of the given box as a tuple
-
-
-
-
 def main():
     while True:
         for event in pygame.event.get():
@@ -36,27 +11,18 @@ def main():
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if s
-            game_screen()
+                #if s
+                pass
+
+            board = Board(9, 9, screen, 'easy')
+            board.draw()
+
         pygame.display.update()
         # to implement
-
-
 
 
 if __name__ == "__main__":
     pygame.init()
     pygame.display.set_caption('Sudoku', os.path.join('assets', 'logo.png'))  # set app title
     screen = pygame.display.set_mode((500, 650))  # set app dimensions
-    board = [
-        [5, 3, 0, 0, 7, 0, 0, 0, 0],
-        [6, 0, 0, 1, 9, 5, 0, 0, 0],
-        [0, 9, 8, 0, 0, 0, 0, 6, 0],
-        [8, 0, 0, 0, 6, 0, 0, 0, 3],
-        [4, 0, 0, 8, 0, 3, 0, 0, 1],
-        [7, 0, 0, 0, 2, 0, 0, 0, 6],
-        [0, 6, 0, 0, 0, 0, 2, 8, 0],
-        [0, 0, 0, 4, 1, 9, 0, 0, 5],
-        [0, 0, 0, 0, 8, 0, 0, 7, 9]
-    ]
     main()  # runs main loop
