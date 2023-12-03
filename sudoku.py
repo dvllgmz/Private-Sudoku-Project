3933ec0a9aba2b712ec69f430dea5b9e821aa098
keatonmcarlton <keatonmcarlton@gmail.com>
2023-12-02 19:57:56 -0500

    added main menu and button gui

--- sudoku.py
+++ sudoku.py
@@ -21,20 +21,69 @@ def main():
                 screen.fill(BACKGROUND_COLOR)
 
                 # title
-                title_text = main_menu_font.render('Sudoku', 0, BOLD_LINE_COLOR)
+                title_text = main_menu_font.render('Sudoku', 1, BOLD_LINE_COLOR)
                 title_rect = title_text.get_rect(center=(250, 150))
                 screen.blit(title_text, title_rect)
 
+                #buttons
+                medium_button = button_font.render('Medium', 1, COLOR_WHITE)
+                medium_button.get_rect(center=(250, 350))
+                medium_surface = pygame.Surface((medium_button.get_size()[0] + 40, medium_button.get_size()[1] + 40))
+                medium_surface.fill(REGULAR_LINE_COLOR)
+                medium_surface.blit(medium_button, (20, 20))
+                # use this line to control text location
+                medium_rectangle = medium_surface.get_rect(center=(250, 350))
 
+                easy_button = button_font.render('Easy', 1, COLOR_WHITE)
+                easy_button.get_rect(center=(250, 250))
+                easy_surface = pygame.Surface((medium_button.get_size()[0] + 40, medium_button.get_size()[1] + 40))
+                easy_surface.fill(BOLD_LINE_COLOR)
+                easy_surface.blit(easy_button, (42, 20))
+                # use this line to control text location
+                easy_rectangle = easy_surface.get_rect(center=(250, 250))
+
+
+                hard_button = button_font.render('Hard', 1, COLOR_WHITE)
+                hard_button.get_rect(center=(250, 450))
+                hard_surface = pygame.Surface((medium_button.get_size()[0] + 40, medium_button.get_size()[1] + 40))
+                hard_surface.fill(COLOR_RED)
+                hard_surface.blit(hard_button, (40, 20))
+                # use this line to control text location
+                hard_rectangle = hard_surface.get_rect(center=(250, 450))
+
+
+                quit_button = button_font.render('Quit', 1, COLOR_WHITE)
+                quit_button.get_rect(center=(250, 550))
+                quit_surface = pygame.Surface((medium_button.get_size()[0] + 40, medium_button.get_size()[1] + 40))
+                quit_surface.fill(COLOR_RED)
+                quit_surface.blit(quit_button, (42, 20))
+                # use this line to control text location
+                quit_rectangle = quit_surface.get_rect(center=(250, 550))
+
+                screen.blit(easy_surface, easy_rectangle)
+                screen.blit(medium_surface, medium_rectangle)
+                screen.blit(hard_surface, hard_rectangle)
+                screen.blit(quit_surface, quit_rectangle)
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        pygame.quit()
+                        sys.exit()
                 if event.type == pygame.MOUSEBUTTONDOWN:
-
-                    #fixme: Finish the home screen and implement board generation\
-
-
-                    board = Board(9, 9, screen, 'easy')
-                    board.draw()
-                    current_screen = 'game'
-                continue
+                    if easy_rectangle.collidepoint(event.pos):
+                        board = Board(9, 9, screen, 'easy')
+                        board.draw()
+                        current_screen = 'game'
+                    elif medium_rectangle.collidepoint(event.pos):
+                        board = Board(9, 9, screen, 'medium')
+                        board.draw()
+                        current_screen = 'game'
+                    elif hard_rectangle.collidepoint(event.pos):
+                        board = Board(9, 9, screen, 'hard')
+                        board.draw()
+                        current_screen = 'game'
+                    elif quit_rectangle.collidepoint(event.pos):
+                        pygame.quit()
+                        sys.exit()
 
             if current_screen == 'game':
                 if event.type == pygame.MOUSEBUTTONDOWN:
@@ -71,6 +120,7 @@ def main():
                         board.clear()
                     if event.key == pygame.K_ESCAPE:
                         pygame.quit()
+                        sys.exit()
                     board.draw()
 
                 continue
